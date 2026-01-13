6f2b63d1db2c184701ebac6b0ab71414ccc12810
Its-Vaibhav-2005 <pandey.vaibhav311005@gmail.com>
2026-01-14 00:05:39 +0530

    Multer Setup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import multer from "multer";
 import {v4 as uuidv4} from "uuid";
+import path from "path";
 
 const app = express();
 
@@ -14,6 +15,32 @@ app.use(cors({
     credentials: true
 }))
 // Custome Middleware
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    next()
+})
+
+// Express Milldleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+// Uploads Configuration
+app.use("/uploads", express.static("uploads"));
+
+// Multer Middleware
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./uploads")
+    },
+    filename: function(req, file, cb){
+        cb(null, file.fieldname+"-"+uuidv4()+path.extname(file.originalname))
+    }
+})
+
+// Multer Configuration
+const upload = multer({
+    storage: storage,
+})
 
 app.get('/', (req, res)=>{
     res.json({msg:"Video Streaming Server is Running"});
